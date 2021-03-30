04bca48ba5a493c59017a0992edc3edf4350bce1
niki-sal <ne.salehi@gmail.com>
2021-03-30 10:54:50 -0700

    Setting up the APIs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from stickynotes import views
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BookView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
